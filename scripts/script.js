ddd9f762ea2b22c42f394892daa3bf3d50196b0b
Frenetz <xodyrev.fedya@mail.ru>
2023-07-23 22:49:11 +0300

    Final changes in CSS and JS code

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
+const field = document.querySelector(".container");
 const button = document.querySelector(".color-button");
+const text = document.getElementById("current-color");
+let color;
+
+function changeColor() {
+  let rand1 = Math.floor(Math.random() * (255 - 0) + 1).toString(16);
+  let rand2 = Math.floor(Math.random() * (255 - 0) + 1).toString(16);
+  let rand3 = Math.floor(Math.random() * (255 - 0) + 1).toString(16);
+  field.style.backgroundColor = `#${rand1}${rand2}${rand3}`;
+  text.textContent = ` #${rand1}${rand2}${rand3}`.toUpperCase();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  changeColor();
+});
+
+button.addEventListener("click", function (event) {
+  changeColor();
+});
